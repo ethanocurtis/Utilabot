a9791774c99c195d751568f31bfb3cafdf134839
ethanocurtis <ethan@curtwurk.com>
2025-08-10 16:31:15 -0500

    update bot.py

--- bot.py
+++ bot.py
@@ -322,7 +322,10 @@ class Store:
         return data["trivia"].get("token")
 
     def set_trivia_token(self, token: Optional[str]):
-
+    def set_trivia_token(self, token: Optional[str]):
+        data = self.read()
+        data['trivia']['token'] = token
+        self.write(data)
     # Weather defaults & subscriptions
     def set_user_zip(self, user_id: int, zip_code: str):
         data = self.read()
